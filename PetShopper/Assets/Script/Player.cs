90d1f7707816ad20ef761d2abf453b835d34f3f5
Ranedr0p <dkdecast@uci.edu>
2025-04-17 18:17:49 -0700

    Move
    Made the player move and sprint and yup

--- PetShopper/Assets/Script/Player.cs
+++ PetShopper/Assets/Script/Player.cs
@@ -2,15 +2,26 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private Transform _playerTransform;
+    public float _speed = 2.0f;
+    public float sprint = 7.0f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        _playerTransform = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _playerTransform.Translate(Input.GetAxisRaw("Horizontal") * _speed * Time.deltaTime, Input.GetAxisRaw("Vertical") * _speed * Time.deltaTime, 0);
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            _speed = sprint; 
+        }
+        else{
+            _speed = 2.0f; 
+        }
     }
 }
