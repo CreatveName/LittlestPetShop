5489b76dde875b19c1fd84beb79ff69eaf317c71
Ranedr0p <dkdecast@uci.edu>
2025-04-20 13:21:26 -0700

    Movement
    Got rid of diagonal movement.

--- PetShopper/Assets/Script/Player.cs
+++ PetShopper/Assets/Script/Player.cs
@@ -2,25 +2,27 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private Transform _playerTransform;
     public float _speed = 2.0f;
     public float _sprint = 7.0f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        _playerTransform = GetComponent<Transform>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        _playerTransform.Translate(Input.GetAxisRaw("Horizontal") * _speed * Time.deltaTime, Input.GetAxisRaw("Vertical") * _speed * Time.deltaTime, 0);
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
-            _speed = _sprint; 
+        if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1){
+            transform.Translate(0, Input.GetAxisRaw("Vertical") * _speed * Time.deltaTime, 0);
+        }else if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1){
+            transform.Translate(Input.GetAxisRaw("Horizontal") * _speed * Time.deltaTime, 0, 0); 
         }
-        else{
+       
+        if (Input.GetKey(KeyCode.LeftShift)){
+            _speed = _sprint; 
+        }else{
             _speed = 2.0f; 
         }
     }
