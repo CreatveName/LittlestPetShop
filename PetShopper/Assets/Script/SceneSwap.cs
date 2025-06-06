8830e828851eeecb2e28bec251bf2237b478d7ce
SomeGamerDude <ragingduo@gmail.com>
2025-06-05 18:25:52 -0700

    Demo Stuff

--- PetShopper/Assets/Script/SceneSwap.cs
+++ PetShopper/Assets/Script/SceneSwap.cs
@@ -11,6 +11,9 @@ public class SceneSwap : MonoBehaviour
     public string PlayerTag = "Player";
     public string SceneName;
 
+    [SerializeField]
+    private float timeToWait;
+
     private void OnTriggerEnter2D(Collider2D other) 
     {
         if(other.CompareTag(PlayerTag))
@@ -23,7 +26,7 @@ public class SceneSwap : MonoBehaviour
     {
         sceneAnim.SetTrigger("Start");
 
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(timeToWait);
 
         SceneManager.LoadSceneAsync(SceneName);
     }
